f14cebf7a7d2a3bb2d82ff10db91c7d2c6b74cdd
Ryan Arya Pratama <ryan.arya.pratama@gmail.com>
2019-04-17 21:28:44 +0800

    feat: move world to rya_geometric_2
    Change starting location, pickups, and dropoff to values that make sense in the new world

--- test_nav.sh
+++ test_nav.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/robond/catkin_ws/src/turtlebot_simulator/turtlebot_gazebo/worlds/playground.world" &
+#xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/robond/catkin_ws/src/turtlebot_simulator/turtlebot_gazebo/worlds/playground.world" &
+xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 3
 xterm -e "roslaunch turtlebot_gazebo amcl_demo_rya.launch" &
 sleep 3
